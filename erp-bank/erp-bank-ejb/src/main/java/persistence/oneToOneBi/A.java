dc550276e1021de3b204b382bfbaf022c40050c9
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-09-30 16:49:56 +0200

    bank training project first commit
    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

---
+++ erp-bank/erp-bank-ejb/src/main/java/persistence/oneToOneBi/A.java
@@ -0,0 +1,44 @@
+package persistence.oneToOneBi;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Entity implementation class for Entity: A
+ *
+ */
+@Entity
+
+public class A implements Serializable {
+
+	@Id
+	private int idA;
+
+	@OneToOne(mappedBy = "a")
+	private B b;
+	private static final long serialVersionUID = 1L;
+
+	public A() {
+		super();
+	}
+
+	public int getIdA() {
+		return this.idA;
+	}
+
+	public void setIdA(int idA) {
+		this.idA = idA;
+	}
+
+	public B getB() {
+		return b;
+	}
+
+	public void setB(B b) {
+		this.b = b;
+	}
+
+}
